f3e1decf3e42f320c8484ff3520ef1e509e9269f
Steef-0x <125138667+Steef-0x@users.noreply.github.com>
2023-03-31 13:25:52 +0000

    Update script.js

--- contactform/script.js
+++ contactform/script.js
@@ -1,1 +1,12 @@
-
+//script.js
+function SendMail() {
+	 var params = {
+	 	from_name : document.getElementById("fullName").value,
+	 	email_id : document.getElementById("email_id").value,
+	 	message : document.getElementById("message").value,
+	 }
+	 emailjs.send('service_e6v74xv', 'template_klx3hps', params).then(function(res) {
+	 	alert("Success!! " + res.status);
+	 })
+  
+}
